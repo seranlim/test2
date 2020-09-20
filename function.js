a39638ebadde327bff61b001925cd617e2614fb1
seranlim <63628381+seranlim@users.noreply.github.com>
2020-09-20 19:11:10 +0900

    Update and rename post.js to function.js

--- function.js
+++ function.js
@@ -6,19 +6,19 @@ function check_ok(){
 	}
 	if(document.form.title.value.length==0){
 		alert("제목을 입력하세요.");
-		form.b_title.focus();
+		form.title.focus();
 		return false;
 	}
 	
 	if(document.form.content.value.length==0){
 		alert("내용을 입력하세요.");
-		form.b_content.focus();
+		form.content.focus();
 		return false;
 	}
 	
-	if(document.form.pwd.value.length==0){
+	if(document.form.password.value.length==0){
 		alert("비밀번호를 입력하세요.");
-		form.b_pwd.focus();
+		form.password.focus();
 		return false;
 	}
 	
@@ -26,9 +26,9 @@ function check_ok(){
 }
 
 function delete_ok() {
-		if(document.form.b_pwd.value.length==0){
+		if(document.form.password.value.length==0){
 			alert("비밀번호를 입력하세요.");
-			form.b_pwd.focus();
+			form.password.focus();
 			return false;
 		}
 		document.form.submit();
